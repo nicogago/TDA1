84628abc965ec326fdd690b7a1839af562bdc97b
martincura <martincura@gmail.com>
2016-11-13 13:02:17 -0300

    Problema de la mochila
    Obtiene el profit óptimo, no X.
    
    - Actualizo README con link a informe.

--- README.md
+++ README.md
@@ -12,4 +12,4 @@ https://docs.google.com/document/d/18UHz7Cy12wZ4Hncq5FpJU61feROUh7fYBzjM0CHaaKE
 https://algoritmos-rw.github.io/tda/tp2.html
 
 ### Informe
-{..}
+https://docs.google.com/document/d/1eYH8BBi5IJTfzdB5tty9Wk74dKl26wd_8oFLeC296Ss
--- tp2/Mochila/mochila.py
+++ tp2/Mochila/mochila.py
@@ -4,47 +4,87 @@
 ###  Problema de la mochila  ###
 ###                          ###
 import csv
+import time
+from os import listdir
+from os.path import isfile, join
 
 # a: array, p: profit, v: value, w: weight
 
 # Sin programación dinámica
-def P_dull(i,w,aw,av):
+def P_dull(i, w):
     if i < 0:
         return 0
-    if w < aw[i]:
-        return P_dull(i-1,w,aw,av)
-    return max( P_dull(i-1, w, aw, av),  P_dull(i-1, w-aw[i], aw, av) + av[i] )
+    if aw[i] > w:
+        return P_dull(i-1,w)
+    return max( P_dull(i-1, w),  P_dull(i-1, w-aw[i]) + av[i] )
+
+def knapsack_dull(N, W):
+    return P_dull(N, W)
 
 
-weight_list = [1,2,3,4]
-values_list = [4,3,2,1]
-print P_dull(3,8,weight_list,values_list)
+def knapsack(N, W):
+    M = [[0 for p in xrange(W+1)] for p in xrange(N+1)]
 
-with open('smallcoeff_pisinger/knapPI_9_50_1000.csv') as csvfile:
-    reader = csv.reader(csvfile, delimiter=' ')
-    seguir = True
+    def P(i, w):
+        if i <= 0 or w == 0:
+            return 0
+        if aw[i] > w:
+            return M[i-1][w]
+        return max( M[i-1][w], M[i-1][w-aw[i]] + av[i] )
 
-    try:    # Medio feo pero sirve
-        while (seguir):
-            test_name = reader.next()[0]
-            N = reader.next()[1]
-            W = reader.next()[1]
-            z = reader.next()[1]
-            t = reader.next()[1]
-            vl = []
-            wl = []
-            xl = []
-            for row in reader:
-                if "--" in row[0]:
-                    break
-                (i, vi, wi, xi) = map(int, row[0].split(','))
-                vl.append(vi)
-                wl.append(wi)
-                xl.append(xi)
-            print test_name, N, W, z, t
-            reader.next()
+    for i in xrange(1, N+1):
+        for w in xrange(1, W+1):
+            M[i][w] = P(i, w)
+    return M[N][W]
 
-            # P()
 
-    except StopIteration:
-        print "-----"
+dirname = "smallcoeff_pisinger"
+# dirname = "hardinstances_pisinger"
+
+files = [f for f in listdir(dirname) if isfile(join(dirname, f))]
+for filename in files:
+    if ".csv" not in filename:
+       continue
+
+    with open(join(dirname, filename)) as csvfile:
+        print "--------------"
+        print filename
+        reader = csv.reader(csvfile, delimiter=' ')
+        seguir = True
+
+        try:
+            while (seguir):
+                test_name = reader.next()[0]
+                N = int(reader.next()[1])
+                W = int(reader.next()[1])
+                z = int(reader.next()[1])
+                t = float(reader.next()[1])
+                av = [0]
+                aw = [0]
+                xl = [0]
+                for row in reader:
+                    if "--" in row[0]:
+                        break
+                    (i, vi, wi, xi) = map(int, row[0].split(','))
+                    av.append(vi)
+                    aw.append(wi)
+                    xl.append(xi)
+                reader.next()  # Ignora línea vacía tras cada test
+
+                print test_name, '\t', "size(M) =", (N+1)*(W+1), '\t',
+                start = time.clock()
+
+                k = knapsack(N, W)
+
+                elapsed = time.clock() - start
+
+                if k != z:
+                    print
+                    print k, "!=", z, "!!!"
+                    assert k == z
+
+                print elapsed, "s\t(vs", t, "s)"
+
+        except StopIteration:
+            print "--- passed ---"
+            print
