73c9249b22dbc2c68f5d60725e892d004e3417a5
emanuelcondo <emanuelcondo@gmail.com>
2016-11-18 11:56:02 -0300

    Agrego print del recorrido del viajante

--- tp2/Viajante/viajante.py
+++ tp2/Viajante/viajante.py
@@ -7,13 +7,15 @@ def viajante(inicio, M):
     v = inicio - 1
     S.remove(v)
     costos = {}
+    parents = {}
+    rec = []
 
     def D(x, conjunto):
         if (str(x)+str(conjunto)) in  costos:
             return costos[str(x)+str(conjunto)]
 
         if (len(conjunto) == 0):
-            print 'g(', x, ',[]) = ', M[x][v]
+#            print 'g(', x, ',[]) = ', M[x][v]
             return M[x][v]
 
         values = []
@@ -21,8 +23,26 @@ def viajante(inicio, M):
             subconjunto = list(conjunto)
             subconjunto.remove(elem)
             values.append(M[x][elem] + D(elem, subconjunto))
-        print 'g(', x, ',',conjunto,') = ', min(values)
-        costos[str(x)+str(conjunto)] = min(values)
+#        print 'g(', x, ',',conjunto,') = ', min(values)
+        costo_min = min(values)
+        costos[str(x)+str(conjunto)] = costo_min
+        prox_vert_cercano = conjunto[values.index(costo_min)]
+        parents[str(x)+str(conjunto)] = prox_vert_cercano
         return min(values)
 
-    return D(v, S)
+    def recorrido(x, conjunto):
+        if len(conjunto) == 0:
+            return
+        vertice = parents[str(x)+str(conjunto)]
+#        print 'p(', x, ',', str(conjunto),') = ', vertice
+        rec.insert(0, vertice+1)
+        subconjunto = list(conjunto)
+        subconjunto.remove(vertice)
+        recorrido(vertice, subconjunto)
+
+    costoTotal = D(v, S)
+    recorrido(v, S)
+    rec.insert(0, v+1)
+    rec.append(v+1)
+    print rec
+    return costoTotal
