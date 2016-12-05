e4f938790b9407f62cf7c13b27694fb0851390fa
martincura <martincura@gmail.com>
2016-12-05 05:24:20 -0300

    Mochila TP3: Redondeado para entrega

--- tp3/Mochila/mochila.py
+++ tp3/Mochila/mochila.py
@@ -10,52 +10,46 @@ import math
 from os import listdir
 from os.path import isfile, join
 
-##############################################
-def testing(N):
-    # Valores reducidos
-    #e = 0.1
-    #b = (e / (2 * N)) * max(av)
-    b = 10
-    print "e: ", b * (2*N) / max(av)
-    print max(av), b, N, len(av)
-    ava = [int(math.ceil(v/b)) for v in av]
-    print av
-    print ava
-
-
 def knapsack_approx(N, W):
     # Valores reducidos
-    #e = 0.1
-    #b = (e / (2 * N)) * max(av)
-    b = 10
+    e = 0.1
+    b = (e / (2 * N)) * max(av)
+    # Valor artificial para limitar M
+    # de forma que mi computadora no explote
+    b_max = sum(av) * N / 100000000
+    b = max([b, b_max])
+    if b < 1:
+        b = 1
+    e = b * (2 * N) / max(av)
+
     ava = [int(math.ceil(v/b)) for v in av]
-
     V = sum(ava)
-    M = [[0 for p in xrange(V+1)] for p in xrange(N+1)]
-    print "N =", N, "V =", V, "size M =", (V+1)*(N*1), "\t",
+    print "b =", b, "| e =", e, "|",
+    print "V~ =", V, "| s(M) =", (N+1)*(V+1), "\t",
     sys.stdout.flush()
-    #time.sleep(20)
 
-    def P(i, v, suma_parcial):
-        if i <= 0 or v == 0:
+    M = [[0 for p in xrange(V+1)] for p in xrange(N+1)]
+
+    def P(i, v, suma_anteriores):
+        if v <= 0:
             return 0
-        if v > suma_parcial:
+        if i == 0:
+            return W+1  # representa infinito
+        if v > suma_anteriores:
             return aw[i] + M[i-1][max(0,v-ava[i])]
         return min( M[i-1][v], aw[i] + M[i-1][max(0,v-ava[i])] )
-    # Algo que hacer para considerar elementos aw[i] > W?
 
     for i in xrange(1, N+1):
-        suma_parcial = sum(ava[1:i-1])
-        for v in xrange(1, suma_parcial + ava[i]):
-            #print "i", i, ", v", v, ", sp", suma_parcial
-            M[i][v] = P(i, v, suma_parcial)
+        suma_hasta_anterior = sum(ava[1:i-1])
+        for v in xrange(1, suma_hasta_anterior + ava[i]):
+            M[i][v] = P(i, v, suma_hasta_anterior)
 
     for v in reversed(xrange(V+1)):
-        print v##
         if M[N][v] <= W:
-            print "M[N][v] =", M[N][v], "W =", W, "v =", v,
-            return v
-
+            sol = b*v
+            if not 0 < sol <= (1 + e) * z:
+                print "No!\t",
+            return sol
 
 
 dirname = "smallcoeff_pisinger"
@@ -64,7 +58,7 @@ dirname = "smallcoeff_pisinger"
 files = [f for f in listdir(dirname) if isfile(join(dirname, f))]
 for filename in files:
     if ".csv" not in filename:
-       continue
+        continue
 
     with open(join(dirname, filename)) as csvfile:
         print "---------------"
@@ -91,25 +85,19 @@ for filename in files:
                     aw.append(wi)
                     xl.append(xi)
                     if wi > W:
-                        print "hay casos wi > W"
+                        print "hay casos wi > W" ##
                 reader.next()  # Ignora línea vacía tras cada test
 
                 V = sum(av)
-                print test_name, '\t',# "N*V =", (N+1) * (V+1), "\t ",
+                print test_name, '\t', "N*V =", (N+1) * (V+1), "\t",
                 sys.stdout.flush()
                 start = time.clock()
 
-                print z
                 k = knapsack_approx(N, W)
-                print k
-                #exit(0)
 
                 elapsed = time.clock() - start
-
-                print "best: ", z, "\tapprox: ", k, "\t",
-
-                print elapsed, "s\t(vs", t, "s)\t",
-                print "t / N*W =", elapsed / ((N+1) * (W+1))
+                print "approx =", k, "| expect =", z, "| app/exp=", k/z, "\t",
+                print elapsed, "s\t(vs", t, "s)\t"
 
         except StopIteration:
             print "----- eof -----"
