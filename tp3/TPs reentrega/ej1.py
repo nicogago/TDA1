84f8184e3fe0dbfe778a3e62e0f461efc869e394
fernandonitz <nitzfernando@gmail.com>
2016-12-24 11:21:44 -0300

    ahora chequea si el ciclo es simple

--- tp3/TPs reentrega/ej1.py
+++ tp3/TPs reentrega/ej1.py
@@ -1,29 +1,47 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import copy
 from tadgrafo3 import *
 
+def esCicloSimple(lVertices):
+	# lVertices: lista de elementos
+	# se debe chequear si hay elementos repetidos en la lista
+	elem = lVertices.pop()
+	if lVertices:
+		if elem in lVertices:
+			return False
+		else:
+			if not esCicloSimple(lVertices):
+				return False
+	return True	 
+
 def verificador_zwc(grafo, vertices):     
-    # grafo: digrafo. 
-    # vertices: lista de elementos ordenados en donde el ultimo
-    #           elemento se conecta con el primero para terminar 
-    #           el ciclo.
-    # ej de vertices [1,2,3,4] y representa: 1->2->3->4->1.
-    elem0 = vertices[0]
-    elemAnt = elem0
-    sumatoria = 0
-    for elem in vertices:
-        if elem != elem0:
-            arista = grafo.get_A(elemAnt,elem)
-            if arista == None:
-                return false
-            sumatoria = sumatoria + arista.get_weight()
-            elemAnt = elem
-    arista = grafo.get_A(elem,elem0)
-    sumatoria = sumatoria + arista.get_weight()
-    print (sumatoria)
-    if sumatoria == 0:
-        return True
-    return False
+	# grafo: digrafo. 
+	# vertices: lista de elementos ordenados en donde el ultimo
+	#           elemento se conecta con el primero para terminar 
+	#           el ciclo.
+	# ej de vertices [1,2,3,4] y representa: 1->2->3->4->1.
+	aux = copy.deepcopy(vertices)
+	if esCicloSimple(aux):
+		elem0 = vertices[0]
+		elemAnt = elem0
+		sumatoria = 0
+		for elem in vertices:
+			if elem != elem0:
+				arista = grafo.get_A(elemAnt,elem)
+				if arista == None:
+					return false
+				sumatoria = sumatoria + arista.get_weight()
+				elemAnt = elem
+		arista = grafo.get_A(elem,elem0)
+		sumatoria = sumatoria + arista.get_weight()
+		print (sumatoria)
+		if sumatoria == 0:
+			return True
+		return False
+	else:
+		print ("No es un ciclo simple...")
+		return False
 
 g = Digraph(6)
 g.add_edge(0,1,3)
@@ -39,8 +57,20 @@ g.add_edge(3,0,-8)
 
 verticesT = [0,5,4,3]
 verticesF = [0,1,2]
+verticesFC= [0,5,4,3,0,5,4,3]
 
 res = verificador_zwc(g, verticesT)
 print (res)
 res = verificador_zwc(g, verticesF)
 print (res)
+res = verificador_zwc(g, verticesFC)
+print (res)
+
+"""
+cTrue = [0,5,4,3]
+cFalse = [0,1,2,3,2,5]
+res = esCicloSimple(cTrue)
+print (res)
+res = esCicloSimple(cFalse)
+print (res)
+"""
